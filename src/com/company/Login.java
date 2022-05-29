0f2982af0a76158b9de80a3ba61a1c5bcf363c74
johnnycarvalhodev <johnnycarvalhodev@gmail.com>
2022-05-28 22:20:59 -0300

    commit

--- src/com/company/Login.java
+++ src/com/company/Login.java
@@ -14,35 +14,69 @@ public class Login {
         // Create a variable for umputting data
         Scanner input = new Scanner(System.in);
 
-        System.out.print(color+"31"+"m"+"*** REGISTER PAGE ***"+color+"m");
+        System.out.println(color+"31"+"m"+"*** REGISTER PAGE ***"+color+"m");
 
-        System.out.println("What do you wnat to register?");
+        System.out.println("Oque deseja registrar?");
 
-        System.out.print("Write 1 for car or 2 for user");
+        System.out.print("\nDigite 1 para carro e 2 para usuário");
         Integer register = input.nextInt();input.nextLine();
 
 
 
 
         if (register == 1) {
-            System.out.print("Enter your ID: ");
+
+            System.out.println(color+"32"+"m"+"DIGITE OS DADOS ABAIXO CONFORME REQUERIDO"+color+"m");
+
+            System.out.print("Código: ");
             newCars.setID(input.nextInt());input.nextLine();
 
-            System.out.print("Enter with the document number car: ");
+            System.out.print("Documento: ");
             newCars.setNumberDocument(input.nextInt());input.nextLine();
 
-            System.out.print("Enter your car name: ");
+            System.out.print("Marca: ");
             newCars.setName(input.nextLine());
 
-            System.out.print("Enter your car brand: ");
+            System.out.print("Modelo: ");
             newCars.setCarBrand(input.nextLine());
 
-            System.out.println("Enter with model/year car");
+            System.out.print("Digite o ano/modelo do carro: ");
             newCars.setModelYear(input.nextInt());input.nextLine();
 
         }
         else if (register == 2) {
-            
+
+            System.out.println(color+"35"+"m"+"DIGITE OS DADOS ABAIXO CONFORME REQUERIDO"+color+"m");
+
+            System.out.print("Id usuário (somente números): ");
+            newUsers.setID(input.nextInt());input.nextLine();
+
+            System.out.print("CPF: ");
+            newUsers.setNumberDocument(input.nextInt());input.nextLine();
+
+            System.out.print("Nome completo: ");
+            newUsers.setName(input.nextLine());
+
+            System.out.print("E-mail: ");
+            newUsers.setEmail(input.nextLine());
+
+            System.out.print("Telefone: ");
+            newUsers.setNumberPhone(input.nextInt());input.nextLine();
+
+            System.out.print("Rua: ");
+            newUsers.setStreet(input.nextLine());
+
+            System.out.print("Nº da residência: ");
+            newUsers.setNumberHome(input.nextInt());input.nextLine();
+
+            System.out.print("Cidade: ");
+            newUsers.setCity(input.nextLine());
+
+            System.out.print("CEP: ");
+            newUsers.setZipCode(input.nextInt());input.nextLine();
+
+            System.out.print("Ano nascimento: ");
+            newUsers.setBirthDate(input.nextInt());input.nextLine();
         }
     }
 }
